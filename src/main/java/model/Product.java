4c01c4a50beedb6c5a47ac3b8466e8c259ee0a87
pedago <pedago@pcalb-mm0407.univ-jfc.fr>
2019-11-27 15:47:30 +0100

    Ajout de la classe Product

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author pedago
+ */
+public class Product {
+    private int ref;
+    private String nom;
+    private int fournisseur;
+    private int categorie;
+    private String quantite;
+    private float prix_uni;
+    private int unites_en_stock;
+    private int unites_commandees;
+    private int niveau_de_reappro;
+    private int disponibilite;
+    
+    public Product(int r,String name,int fourni,int cate,String qte,float prix,int stock,int commandees,int reappro,int dispo){
+        this.ref=r;
+        this.nom=name;
+        this.fournisseur=fourni;
+        this.categorie=cate;
+        this.quantite=qte;
+        this.prix_uni=prix;
+        this.unites_en_stock=stock;
+        this.unites_commandees=commandees;
+        this.niveau_de_reappro=reappro;
+        this.disponibilite=dispo;
+    }
+    
+    
+    
+}

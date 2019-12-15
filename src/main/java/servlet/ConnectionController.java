71c3d10135a0b4ca820f4f1b0c1d55c1659a17d3
Gourgues <Gourgues@LAPTOP-VMF5MHCF>
2019-12-16 00:54:24 +0100

    session admin

--- src/main/java/servlet/ConnectionController.java
+++ src/main/java/servlet/ConnectionController.java
@@ -98,9 +98,6 @@ public class ConnectionController extends HttpServlet {
                 request.setAttribute("message",error);             
                 request.getRequestDispatcher(jspView).forward(request, response);
             }   
-            
-            
-            //request.getRequestDispatcher(jspView).forward(request, response);
         } catch (SQLException ex) {
             Logger.getLogger(ConnectionController.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -115,86 +112,4 @@ public class ConnectionController extends HttpServlet {
                 response.sendRedirect("Connexion.jsp");
         
     }
-    
-
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-/*
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-*/
-/*
-    private void checkLogin(HttpServletRequest request) throws SQLException {
-        System.out.print("checkingLogin");
-        DAO dao = new DAO(DataSourceFactory.getDataSource());
-
-        // Les paramètres transmis dans la requête
-        String login = request.getParameter("login");
-        String password = request.getParameter("password");
-
-*/        /*Le login/password défini dans web.xml*/
-/*        String loginAd = getInitParameter("loginAdmin");
-        String passwordAd = getInitParameter("passwordAdmin");
-        String userName = getInitParameter("ID");
-
-        if (loginAd.equals(login) && passwordAd.equals(password)) {
-            HttpSession session = request.getSession(true); // démarre la session
-            session.setAttribute("userAdmin", userName);
-            System.out.println("Admin");
-        } else if (dao.connexionClient(login, password)) {
-            // On a trouvé la combinaison login / password
-            // On stocke l'information dans la session
-            HttpSession session = request.getSession(true); // démarre la session
-            //Nom du client (Non admin)
-            String name = login;
-            session.setAttribute("userName", name);
-            session.setAttribute("id", password);
-            System.out.println("User");
-        } else { // On positionne un message d'erreur pour l'afficher dans la JSP
-            request.setAttribute("errorMessage", "Login/Password incorrect");
-            System.out.println("Fail");
-        }
-    }
-
-    private void doLogout(HttpServletRequest request) {
-        // On termine la session
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
-    }
-
-    private String findUserInSession(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        return (session == null) ? null : (String) session.getAttribute("userName");
-    }
-
-    private String findAdminInSession(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        return (session == null) ? null : (String) session.getAttribute("userAdmin");
-    }
-
-    /*private void AjouterCommande(HttpServletRequest request) throws SQLException {
-        DAO dao = new DAO(DataSourceFactory.getDataSource());
-
-        int quantite = Integer.parseInt(request.getParameter("quantite"));
-        int id = Integer.parseInt(request.getParameter("id"));
-        float fraisport = Float.parseFloat(request.getParameter("fraisport"));
-        String dateAchat = request.getParameter("dateAchat");
-        String dateLivraison = request.getParameter("dateLivraison");
-        String description = request.getParameter("produit");
-
-        PurchaseEntity commande = new PurchaseEntity(1, id, quantite, 30, fraisport, dateAchat, dateLivraison, description);
-
-        dao.ajoutCommande(commande);
-
-    }*/
-    
-
 }
